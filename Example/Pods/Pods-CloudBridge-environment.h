977fe4538d4782c5ffddd2ca8224d38f0d239f37
Oliver Letterer <oliver.letterer@gmail.com>
2014-08-13 09:58:44 +0200

    pod update.

--- Example/Pods/Pods-CloudBridge-environment.h
+++ Example/Pods/Pods-CloudBridge-environment.h
@@ -16,7 +16,7 @@
 #define COCOAPODS_POD_AVAILABLE_CloudBridge
 #define COCOAPODS_VERSION_MAJOR_CloudBridge 0
 #define COCOAPODS_VERSION_MINOR_CloudBridge 5
-#define COCOAPODS_VERSION_PATCH_CloudBridge 0
+#define COCOAPODS_VERSION_PATCH_CloudBridge 1
 
 // Expecta
 #define COCOAPODS_POD_AVAILABLE_Expecta
